3339fc46c834e6e3d22be9eccfc541928040aa5c
treymid <26smithtrea@washk12.org>
2025-02-27 11:41:48 -0700

    madleds

--- madlibs.js
+++ madlibs.js
@@ -1,5 +1,9 @@
 const prompt = require("prompt-sync")(); // Ask for user inputs
 let gameCharacter = prompt("Enter a game character: ");
 let weapon = prompt("Enter a weapon: ");
-let story = "Once upon a time " + gameCharacter + " was on a mission to ..."
+let age = prompt("enter character age " )
+let gameCharactermiddelname = prompt("enter character middel name ")
+let gameCharacterlastname = prompt("enter character last name ")
+let job = prompt("enter character job ")
+let story = "Once upon a time " + gameCharacter + gameCharactermiddelname + gameCharactermiddelname + "a" +age+job+ " was on a mission to ..."
 console.log(story);
